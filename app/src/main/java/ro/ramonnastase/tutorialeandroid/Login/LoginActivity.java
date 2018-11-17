5b011a3638ed7dc8babd543daa4d29ce7b502b67
= <mako_edvin@yahoo.com>
2018-11-17 18:16:46 +0100

    Tutorial 5

--- app/src/main/java/ro/ramonnastase/tutorialeandroid/Login/LoginActivity.java
+++ app/src/main/java/ro/ramonnastase/tutorialeandroid/Login/LoginActivity.java
@@ -18,15 +18,6 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout_login_main, new LoginFragment()).commit();
     }
 
